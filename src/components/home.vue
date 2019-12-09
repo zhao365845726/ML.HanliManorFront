2aae1308bd12d5085ce77dece5332b9df5445895
whl1994 <736148701@qq.com>
2019-12-09 09:14:41 +0800

    12.9,首页图片更改

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,19 +1,17 @@
 <template>
   <div class="home">
-    <!--<img src="../assets/img/bg.png" alt="">-->
-    <swiper :options="swiperOption" class='swiper-box'>
-      <swiper-slide><img src="http://image.hanjiazhuang.cn/bg.png" alt=""></swiper-slide>
+    <img src="http://image.hanjiazhuang.cn/%E9%9F%A9%E6%A2%A8%E9%A6%96%E9%A1%B5%E5%9B%BE%E7%89%87.jpg" alt="">
+    <!--<swiper :options="swiperOption" class='swiper-box'>
+      <swiper-slide><img src="http://image.hanjiazhuang.cn/%E9%9F%A9%E6%A2%A8%E9%A6%96%E9%A1%B5%E5%9B%BE%E7%89%87.jpg" alt=""></swiper-slide>
       <swiper-slide><img src="http://image.hanjiazhuang.cn/%E9%A6%96%E9%A1%B5.png" alt=""></swiper-slide>
-      <!--<swiper-slide><img v-for="img in imga" v-lazy="img" alt=""></swiper-slide>
-      <swiper-slide><img v-for="img in imgb" v-lazy="img" alt=""></swiper-slide>-->
-    </swiper>
+    </swiper>-->
     <!--<div class="swiper-button-prev"></div>--><!--左箭头。如果放置在swiper-container外面，需要自定义样式。-->
     <!--<div class="swiper-button-next"></div>--><!--右箭头。如果放置在swiper-container外面，需要自定义样式。-->
 
   </div>
 </template>
 
-<script>
+<!--<script>
 export default {
   name: 'home',
     data() {
@@ -25,9 +23,10 @@ export default {
         //   'http://image.hanjiazhuang.cn/%E9%A6%96%E9%A1%B5.png'
         //],
       msg: '',
-      swiperOption: {
-        loop: true,
+        swiperOption: {
+         loop: false,
         speed: 5000,
+        autoplay: false,
         autoplay: {
           delay:3000
         },
@@ -36,7 +35,7 @@ export default {
     }
     }
 }
-</script>
+</script>-->
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
