206278e8d1bc9bbb805e692af887aba86fc586d6
whl1994 <736148701@qq.com>
2019-12-10 17:07:05 +0800

    12.10.首页图片修改

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img src="http://image.hanjiazhuang.cn/%E9%9F%A9%E6%A2%A8%E9%A6%96%E9%A1%B5%E5%9B%BE%E7%89%87.jpg" alt="">
+    <img src="http://image.hanjiazhuang.cn/%E9%9F%A9%E6%A2%A8%E9%A6%96%E9%A1%B5%E5%9B%BE%E7%89%871.jpg" alt="">
     <!--<swiper :options="swiperOption" class='swiper-box'>
       <swiper-slide><img src="http://image.hanjiazhuang.cn/%E9%9F%A9%E6%A2%A8%E9%A6%96%E9%A1%B5%E5%9B%BE%E7%89%87.jpg" alt=""></swiper-slide>
       <swiper-slide><img src="http://image.hanjiazhuang.cn/%E9%A6%96%E9%A1%B5.png" alt=""></swiper-slide>
